6eb93d67b5bd835588b97d28c20129120e1e4e41
Yahor Matviyevich <cs237224@gmail.com>
2026-04-02 02:55:53 +0300

    requirements and config.example update

--- config.example.py
+++ config.example.py
@@ -2,14 +2,18 @@ GOLDEN_KEY = "PUT_YOUR_GOLDEN_KEY_HERE"
 USER_AGENT = "PUT_YOUR_USER_AGENT_HERE"
 REQUESTS_DELAY = 3
 
+TELEGRAM_ADMIN_BOT_TOKEN = "PUT_YOUR_TELEGRAM_BOT_TOKEN_HERE"
+TELEGRAM_ADMIN_USER_ID = 123456789
+
 WELCOME_TEXT = "🤖 Привет! Я бот-помощник автоответчик. Напиши /help, чтобы увидеть доступные команды."
 HELP_TEXT = (
     "🤖 Доступные команды:\n"
     "/help — список команд\n"
-    "/free — количество свободных аккаунтов с рейтингом\n"
+    "/free — список свободных аккаунтов с рейтингом\n"
     "/acc — данные аккаунта\n"
     "/code — код для входа\n"
-    "/time — время аренды"
+    "/time — время аренды\n"
+    "/admin — позвать продавца"
 )
 
 STEAM_SIGN_OUT_ENABLED = True
